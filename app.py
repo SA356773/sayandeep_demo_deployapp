9111894388f01fe607ec4a76b4c6d9b0c4a6c6c1
SA356773 <47416901+SA356773@users.noreply.github.com>
2019-02-07 18:23:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,18 @@ app = Flask(__name__)
 
 @app.route('/demo',methods=['GET'])
 def demo():
-  return 'Hello World'
+  speech = 'Hello World'
+  my_result =  {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-demo-webhook-sample"
+    }
+  res = json.dumps(my_result, indent=4)
+  r = make_response(res)
+  r.headers['Content-Type'] = 'application/json'
+  return r
   
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
