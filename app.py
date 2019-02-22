5582119c219f1fb8650c0a28a0a573408ffc9b00
SA356773 <47416901+SA356773@users.noreply.github.com>
2019-02-22 17:03:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def createResponse(req):
   return r
   
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+    port = int(os.getenv('PORT', 8080))
 
     print("Starting app on port %d" % port)
 
