03fbbcbb4c3f3f06b65254762b927fe42efcc50c
SA356773 <47416901+SA356773@users.noreply.github.com>
2019-03-09 18:52:17 +0530

    Update app_alexa.py

--- app_alexa.py
+++ app_alexa.py
@@ -1,27 +1,25 @@
 from flask import Flask
 from flask_ask import statement,question,Ask,session
-import requests
 import time
 import os
 
 app = Flask(__name__)
-ask = Ask(app,"/")
+ask = Ask(app,"/webhook")
 	
 @ask.launch
 def handle_launch():
-	print ("Entered")
 	message = "Hello there, how can I help you ?"
 	return question(message)
 
 @ask.intent("AnimalInfoIntent")
-def handle_animalInfoIntent(animal_name):
-	result = "This is demo joke on " + animal_name
+def handle_animalInfoIntent():
+	result = "This is demo joke on animals"
 	return statement(result)
 
 
 @ask.intent("AMAZON.FallbackIntent")
 def handle_fallbackIntent():
-	result = "Sorry, I couldn't get that. Please try again, thank you"
+	result = "Sorry, I couldn't get that, thank you"
 	return statement(result)
 
 if __name__ == '__main__':
